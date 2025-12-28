5cad16b81c4eeaa9dd05f47c57d0a1a494aa5c5a
tieukhaihoan127 <tieukhaihoan127@gmail.com>
2025-12-28 19:46:01 +0700

    Add Font and Home Hero

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
             </div>
             <Link to='/cart' className='relative'>
                 <img src={assets.cart_icon} alt="" className='w-5 min-w-5' />
-                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>
+                <p className='absolute -right-1.25 -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>
                     10
                 </p>
             </Link>
