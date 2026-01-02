d2683589ef489349bd157b9bfe346c697e3c2a29
tieukhaihoan127 <tieukhaihoan127@gmail.com>
2026-01-02 23:13:52 +0700

    Fix Best Seller and Create Policy Session

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -9,7 +9,7 @@ const BestSeller = () => {
     const [bestSeller, setBestSeller] = useState([]);
 
     useEffect(() => {
-        const bestProduct = products.filter((item) => (item.bestSeller));
+        const bestProduct = products.filter((item) => (item.bestseller));
         setBestSeller(bestProduct.slice(0,5));
     },[]);
 
