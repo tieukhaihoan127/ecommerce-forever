09fe1a66b1572a3872fa116bde3377c6d72432a8
tieukhaihoan127 <tieukhaihoan127@gmail.com>
2026-01-05 14:01:59 +0700

    Add Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets } from '../assets/frontend_assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+            <div>
+                <img src={assets.logo} alt="logo" className='mb-5 w-32'/>
+                <p className='w-full md:w-2/3 text-gray-600'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae perspiciatis praesentium ullam dolorum deserunt dolores reprehenderit libero! Natus asperiores eveniet enim obcaecati? Iste consequatur est libero dolorem nostrum at beatae.
+                </p>
+            </div>
+            <div>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@foreveryou.com</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2026@ forever.com - All Right Reserved.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
