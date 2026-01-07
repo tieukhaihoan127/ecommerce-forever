4f4e323d3f6770ebdba15884b0c993b6558da550
tieukhaihoan127 <tieukhaihoan127@gmail.com>
2026-01-07 21:53:06 +0700

    Cart Count

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { toast } from 'react-toastify';
 
 export const ShopContext = createContext();
 
@@ -9,6 +10,53 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(true);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+
+        if(!size) {
+            toast.error('Select Product Size');
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]) {
+            if(cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else {
+                cartData[itemId][size] = 1;
+            }
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+
+        setCartItems(cartData);
+    }
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for(const items in cartItems) {
+            for(const item in cartItems[items]) {
+                try {
+                    if(cartItems[items][item] > 0) {
+                        totalCount+=cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+
+        return totalCount;
+    }
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems]);
 
     const value = {
         products,
@@ -17,7 +65,10 @@ const ShopContextProvider = (props) => {
         search,
         setSearch,
         showSearch,
-        setShowSearch
+        setShowSearch,
+        cartItems,
+        addToCart,
+        getCartCount
     };
 
     return (
