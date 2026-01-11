4b17f5e2a2fbe4224a12aa66940fb858f2fdd455
tieukhaihoan127 <tieukhaihoan127@gmail.com>
2026-01-11 18:32:00 +0700

    Implementing User API

---
+++ backend/controllers/productController.js
@@ -0,0 +1,20 @@
+// Function for add product
+const addProduct = async (req,res) => {
+    
+}
+
+
+// Function for list product
+const listProducts = async (req,res) => {
+
+}
+
+// Function for removing product
+const removeProduct = async (req,res) => {
+
+}
+
+// Function for single product info
+const singleProduct = async (req,res) => {
+    
+}
