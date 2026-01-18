450f290fd0e45125a6e1e37336703cd2e6f8dbf1
tieukhaihoan127 <tieukhaihoan127@gmail.com>
2026-01-18 22:10:40 +0700

    Connect API to UI

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -9,9 +9,9 @@ const BestSeller = () => {
     const [bestSeller, setBestSeller] = useState([]);
 
     useEffect(() => {
-        const bestProduct = products.filter((item) => (item.bestseller));
+        const bestProduct = products.filter((item) => (item.bestSeller));
         setBestSeller(bestProduct.slice(0,5));
-    },[]);
+    },[products]);
 
   return (
     <div className='my-10'>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export const ShopContext = createContext();
 
@@ -9,9 +10,11 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(true);
     const [cartItems, setCartItems] = useState({});
+    const [products, setProducts] = useState([]);
     const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
@@ -77,13 +80,36 @@ const ShopContextProvider = (props) => {
                         totalAmount += (itemInfo.price * cartItems[items][item]);
                     }
                 } catch (error) {
-                    
+                    console.log(error);
+                    toast.error(error.message);
                 }
             }
         }
 
         return totalAmount;
     }
+
+    const getProductsData = async () => {
+        try {
+            
+            const response = await axios.get(backendUrl + '/api/product/list');
+
+            if(response.data.success) {
+                setProducts(response.data.products);
+            }
+            else {
+                toast.error(response.data.message);
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getProductsData();
+    }, []);
 
     const value = {
         products,
@@ -98,7 +124,8 @@ const ShopContextProvider = (props) => {
         getCartCount,
         updateQuantity,
         getCartAmount,
-        navigate
+        navigate,
+        backendUrl
     };
 
     return (
