cde6003d72f22667d36507f632d0708372efde31
tieukhaihoan127 <tieukhaihoan127@gmail.com>
2026-01-21 08:52:44 +0700

    Orders API and UI Implementation

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -165,6 +165,7 @@ const ShopContextProvider = (props) => {
         setShowSearch,
         cartItems,
         addToCart,
+        setCartItems,
         getCartCount,
         updateQuantity,
         getCartAmount,
