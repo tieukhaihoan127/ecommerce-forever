bbb0840d08303ae1c3a280ec549b52f9df23fa08
tieukhaihoan127 <tieukhaihoan127@gmail.com>
2026-01-21 15:45:07 +0700

    Implementing Stripe Checkout

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus } from '../controllers/orderController.js';
+import { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe } from '../controllers/orderController.js';
 import adminAuth from '../middleware/adminAuth.js';
 import authUser from '../middleware/auth.js';
 
@@ -12,5 +12,6 @@ orderRouter.post('/place', authUser, placeOrder);
 orderRouter.post('/stripe', authUser, placeOrderStripe);
 orderRouter.post('/razorpay', authUser, placeOrderRazorpay);
 orderRouter.post('/userorders', authUser, userOrders);
+orderRouter.post('/verifyStripe', authUser, verifyStripe);
 
 export default orderRouter;
